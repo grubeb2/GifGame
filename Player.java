d23d4473bb004494b38353058106b088d5eefd5b
ToastyToes <2chrisgeorge@gmail.com>
2016-11-12 14:19:51 -0500

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,20 @@
+package giffy;
+import java.util.HashSet;
+
+public class Player {
+
+	public Player(){
+		HashSet<String> hand = new HashSet<String>();
+		boolean judge = false;
+		start();
+	}
+	
+	//fills hands with gifs
+	public void start(){
+		
+	}
+	
+	public void addGif(){
+		if
+	}
+}
