ea2f33913c786e14e1b6d21d4ebecfc9c4b376a2
ToastyToes <2chrisgeorge@gmail.com>
2016-11-12 19:28:35 -0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.net.MalformedURLException;
 
 public class Player {
-
+	final int NUM_GIFS_HAND = 5;
 	ArrayList<Gif> hand;
 	boolean judge;
 
@@ -16,6 +16,27 @@ public class Player {
 	public void addGif(Gif gif)
 		throws MalformedURLException
 	{
-		hand.add(gif);
+		for(int i = hand.size(); i < NUM_GIFS_HAND; i++)
+		{
+			hand.add(gif);
+		}
+	}
+	
+	public ArrayList<Gif> submit(ArrayList<Gif> thing){
+		return thing;
+	}
+	
+	public void discard(ArrayList<Gif> thing){
+		for(Gif part: thing)
+			hand.remove(part);
+	}
+	
+	public void isJudge(){
+		judge = true;
+	}
+	
+	public ArrayList<Gif> judgeChoice(ArrayList<ArrayList<Gif>> choices, int n){
+		//ArrayList<Gif> answer = new ArrayList<Gif>();
+		return choices.get(n);
 	}
 }
