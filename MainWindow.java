15eb9e67a915d00449de60de5538f6580a2be5a9
grubeb2 <grubeb2@rpi.edu>
2016-11-15 12:13:47 -0500

    Update MainWindow.java

--- MainWindow.java
+++ MainWindow.java
@@ -11,6 +11,7 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.widgets.Spinner;
 
 
 public class MainWindow {
@@ -23,6 +24,7 @@ public class MainWindow {
 	private Button btnSelect;
 	private Button btnEndTurn;
 	private Button btnUndo;
+	private Slider slider_1;
 
 	/**
 	 * Launch the application.
@@ -61,7 +63,6 @@ public class MainWindow {
 		shell.setText("SWT Application");
 		
 		slider = new Slider(shell, SWT.NONE);
-		slider.setTouchEnabled(true);
 		slider.setMaximum(50);
 		slider.setBounds(190, 664, 380, 15);
 		
@@ -77,7 +78,7 @@ public class MainWindow {
 		
 		text = new Text(shell, SWT.BORDER);
 		text.setEditable(false);
-		text.setBounds(10, 47, 740, 137);
+		text.setBounds(10, 47, 740, 118);
 		
 		text_2 = new Text(shell, SWT.BORDER);
 		text_2.setEditable(false);
@@ -92,6 +93,11 @@ public class MainWindow {
 		btnUndo.setSelection(true);
 		btnUndo.setBounds(217, 719, 95, 28);
 		btnUndo.setText("Undo");
+		
+		slider_1 = new Slider(shell, SWT.NONE);
+		slider_1.setMaximum(60);
+		slider_1.setSelection(40);
+		slider_1.setBounds(230, 171, 300, 28);
 
 	}
 	public Image getLblNewLabelImage() {
@@ -153,5 +159,42 @@ public class MainWindow {
 	}
 	public void setBtnUndoSelection(boolean selection_2) {
 		btnUndo.setSelection(selection_2);
+	}
+	public boolean getSlider_1Enabled() {
+		return slider_1.isEnabled();
+	}
+	public void setSlider_1Enabled(boolean enabled) {
+		slider_1.setEnabled(enabled);
+	}
+	public int getSlider_1Maximum() {
+		return slider_1.getMaximum();
+	}
+	public void setSlider_1Maximum(int maximum_1) {
+		slider_1.setMaximum(maximum_1);
+	}
+	
+	public int getSlider_1PageIncrement() {
+		return slider_1.getPageIncrement();
+	}
+	public void setSlider_1PageIncrement(int pageIncrement) {
+		slider_1.setPageIncrement(pageIncrement);
+	}
+	public int getSlider_1Selection() {
+		return slider_1.getSelection();
+	}
+	public void setSlider_1Selection(int selection_3) {
+		slider_1.setSelection(selection_3);
+	}
+	public int getSliderSelection() {
+		return slider.getSelection();
+	}
+	public void setSliderSelection(int selection_4) {
+		slider.setSelection(selection_4);
+	}
+	public int getSliderPageIncrement() {
+		return slider.getPageIncrement();
+	}
+	public void setSliderPageIncrement(int pageIncrement_1) {
+		slider.setPageIncrement(pageIncrement_1);
 	}
 }
